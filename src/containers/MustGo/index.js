bc0633ad132e9b5128e2c5c4d1451b8afe04fe53
Oleg Sergeev <oleg.sergeev@OSERGEEV.intexsoft.by>
2020-02-12 16:44:11 +0300

    Add some pictures

--- src/containers/MustGo/index.js
+++ src/containers/MustGo/index.js
@@ -21,7 +21,7 @@ export const MustGo = () => {
       <div className="must-go__content-wrap">
         <div className="must-go__content--left">
           <Image
-            src="https://www.tripzaza.com/ru/destinations/files/2017/09/Berlin-e1505798693967.jpg"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQs8dMHkl6D-E7WmI_MNUKzgj8C4fZ32n1t4l7kt83iRMsCwX4d"
             alt="must"
             className="must-go__img"
           />
@@ -29,7 +29,7 @@ export const MustGo = () => {
         <div className="must-go__content--right">
           <div className="must-go__content--right--big">
             <Image
-              src="https://www.tripzaza.com/ru/destinations/files/2017/09/Berlin-e1505798693967.jpg"
+              src="https://www.gpsmycity.com/img/gd/1144.jpg"
               alt="must"
               className="must-go__img"
             />
@@ -37,14 +37,14 @@ export const MustGo = () => {
           <div className="must-go__content--right--small">
             <span>
               <Image
-                src="https://www.tripzaza.com/ru/destinations/files/2017/09/Berlin-e1505798693967.jpg"
+                src="https://100resilientcities.org/wp-content/uploads/2017/06/Paris-hero-crop-e1539202545467.jpg"
                 alt="must"
                 className="must-go__img"
               />
             </span>
             <span>
               <Image
-                src="https://www.tripzaza.com/ru/destinations/files/2017/09/Berlin-e1505798693967.jpg"
+                src="https://www.cia-france.com/media/1558/parcarou1_720x500.jpg"
                 alt="must"
                 className="must-go__img"
               />
